41f31013d70727843915adfce6c522a019b8b713
Haythem Sellami <sellamihaythem96@gmail.com>
2023-07-22 18:51:33 +0700

    init

--- src/PetPark.sol
+++ src/PetPark.sol
@@ -2,5 +2,20 @@
 pragma solidity ^0.8.13;
 
 contract PetPark {
-    
+    enum AnimalType {
+        None,
+        Fish,
+        Cat,
+        Dog,
+        Rabbit,
+        Parrot
+    }
+
+    enum Gender {
+        Male,
+        Female
+    }
+
+    event Added(AnimalType indexed animalType, uint256 count);
+    event Borrowed(AnimalType indexed animalType);
 }
